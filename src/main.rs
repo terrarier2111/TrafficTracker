efd6704c293b927a8499d029329f50ad8741d533
threadexception <hannes.gaumann@outlook.de>
2024-07-14 01:59:28 +0200

    Make it somewhat usable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![feature(duration_constructors)]
 
-// /sys/class/net/eth0/statistics/tx_packets
-
-use std::{fs, path::PathBuf, str::FromStr, sync::{atomic::AtomicU64, Arc, Mutex}, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
-use num_bigint::{BigInt, BigUint, ToBigInt, ToBigUint};
+use std::{fs, path::PathBuf, process::Command, str::FromStr, sync::{Arc, Mutex}, thread, time::{Duration, SystemTime, UNIX_EPOCH}};
+use chrono::Local;
+use num_bigint::{BigUint, ToBigUint};
 
 use serde_derive::{Deserialize, Serialize};
 
@@ -12,14 +11,16 @@ fn main() {
     let config = Config::load();
     let save_ms = config.save_interval_ms;
     let meta2 = meta.clone();
+    log("Started up traffic limiter...");
     // interval saver thread
     thread::spawn(move || {
         let meta = meta2;
         loop {
-            thread::sleep(Duration::from_millis(save_ms));
             let mut meta = meta.lock().unwrap();
             meta.last_saved_bytes = fetch_outbound_bytes().to_string();
             meta.store();
+            drop(meta);
+            thread::sleep(Duration::from_millis(save_ms));
         }
     });
     let meta2 = meta.clone();
@@ -45,10 +46,14 @@ fn main() {
     });
     // byte amount saver
     loop {
-        thread::sleep(Duration::from_millis(config.check_interval_ms));
         let curr_bytes = fetch_outbound_bytes();
         let meta = meta.lock().unwrap();
-        let dist = curr_bytes.clone() - BigUint::from_str(&meta.starting_bytes).unwrap();
+        let starting = BigUint::from_str(&meta.starting_bytes).unwrap();
+        let dist = if &curr_bytes > &starting {
+            curr_bytes.clone() - starting
+        } else {
+            BigUint::ZERO
+        };
         if dist >= config.save_every_n_bytes.to_biguint().unwrap() {
             Meta {
                 reset_at_ms: meta.reset_at_ms,
@@ -57,20 +62,37 @@ fn main() {
             }.store();
         }
         if dist > config.max_bytes.to_biguint().unwrap() {
-            enable_lower_bandwidth(config.lower_limit_bytes);
+            enable_lower_bandwidth(config.lower_limit_bytes, config.burst_buffer_size, config.buffer_latency_ms);
             let sleep_time = meta.reset_at_ms.saturating_sub(current_time_millis() as u64);
             drop(meta);
             thread::sleep(Duration::from_millis(sleep_time));
+        } else {
+            drop(meta);
+        }
+        thread::sleep(Duration::from_millis(config.check_interval_ms));
+    }
+}
+
+fn enable_lower_bandwidth(limit: u64, burst_buffer_size: u64, buffer_latency_ms: u64) {
+    log(&format!("Limiting network traffic to {limit} bytes..."));
+    for interface in fs::read_dir("/sys/class/net").unwrap() {
+        if let Ok(interface) = interface {
+            if let Err(err) = Command::new("sudo").args(["tc", "qdisc", "add", "dev", interface.file_name().to_string_lossy().as_ref(), "root", "tbf", "rate", (limit * 8).to_string().as_str(), "burst", &burst_buffer_size.to_string(), "latency", &buffer_latency_ms.to_string()]).spawn().unwrap().wait() {
+                log(&format!("Error applying restriction: {err}"));
+            }
         }
     }
 }
 
-fn enable_lower_bandwidth(limit: u64) {
-
-}
-
 fn disable_lowered_bandwidth() {
-
+    log("Loosening network traffic restrictions...");
+    for interface in fs::read_dir("/sys/class/net").unwrap() {
+        if let Ok(interface) = interface {
+            if let Err(err) = Command::new("sudo").args(["tc", "qdisc", "del", "dev", interface.file_name().to_string_lossy().as_ref(), "root"]).spawn().unwrap().wait() {
+                log(&format!("Error loosening restriction: {err}"));
+            }
+        }
+    }
 }
 
 fn fetch_outbound_bytes() -> BigUint {
@@ -80,7 +102,9 @@ fn fetch_outbound_bytes() -> BigUint {
             let mut path = interface.path();
             path.push("statistics");
             path.push("tx_bytes");
-            sum += BigUint::from_str(fs::read_to_string(path).unwrap().as_str()).unwrap();
+            let raw = fs::read_to_string(path.clone()).unwrap();
+            // the last character isn't part of the number, so ignore it.
+            sum += BigUint::from_str(&raw[0..(raw.len() - 1)]).unwrap();
         }
     }
     sum
@@ -94,6 +118,8 @@ struct Config {
     capture_timeframe_ms: u64,
     max_bytes: u64,
     lower_limit_bytes: u64,
+    burst_buffer_size: u64,
+    buffer_latency_ms: u64,
 }
 
 impl Config {
@@ -112,6 +138,8 @@ impl Config {
                 capture_timeframe_ms: 1000 * 60 * 60 * 24 * 7,
                 max_bytes: 1024 * 1024 * 1024 * 1024,
                 lower_limit_bytes: 64 * 1024,
+                burst_buffer_size: 4096,
+                buffer_latency_ms: 50,
             };
             fs::write(cfg_path, serde_json::to_string_pretty(&cfg).unwrap()).unwrap();
             return cfg;
@@ -147,6 +175,7 @@ impl Meta {
                 last_saved_bytes: sent_bytes.clone(),
                 starting_bytes: sent_bytes,
             };
+            fs::create_dir_all(cfg_path.parent().unwrap()).unwrap();
             fs::write(cfg_path, serde_json::to_string_pretty(&cfg).unwrap()).unwrap();
             return cfg;
         }
@@ -165,4 +194,8 @@ fn current_time_millis() -> u128 {
     let duration_since_epoch = now.duration_since(UNIX_EPOCH).unwrap();
     let milliseconds = duration_since_epoch.as_millis();
     milliseconds
+}
+
+fn log(val: &str) {
+    println!("[{}] {}", Local::now().format("%Y-%m-%d %H:%M:%S").to_string(), val);
 }
